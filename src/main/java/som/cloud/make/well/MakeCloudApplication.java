a90dcbbbc5926f413843a1785e04be54a10fa0f6
chengcheng <chengcheng@3ctech.cn>
2021-12-24 18:04:14 +0800

    project init

---
+++ src/main/java/som/cloud/make/well/MakeCloudApplication.java
@@ -0,0 +1,13 @@
+package som.cloud.make.well;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/som/cloud/make/well/MakeCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package som.cloud.make.well;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
