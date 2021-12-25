5c6e71d19569a3aef68ef31e42de25288cff0fab
chengcheng <shmily_think@126.com>
2021-12-26 01:13:07 +0800

    restTemplate配合eureka进行负载均衡

--- order-service/src/main/java/som/cloud/make/well/order/web/order/service/OrderService.java
+++ order-service/src/main/java/som/cloud/make/well/order/web/order/service/OrderService.java
@@ -2,21 +2,33 @@ package som.cloud.make.well.order.web.order.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import som.cloud.make.well.order.web.data.dao.OrderDao;
 import som.cloud.make.well.order.web.data.pojo.Order;
+import som.cloud.make.well.order.web.data.pojo.User;
 
 @Service
 public class OrderService {
 
     private OrderDao orderDao;
+    private RestTemplate restTemplate;
 
     @Autowired
     public void setOrderDao(OrderDao orderDao) {
         this.orderDao = orderDao;
     }
 
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     public Order queryOrderById(Long orderId) {
-        return orderDao.findById(orderId);
+        Order order = orderDao.findById(orderId);
+        String url = "http://user-service/user/" + order.getUserId();
+        User user = restTemplate.getForObject(url, User.class);
+        order.setUser(user);
+        return order;
     }
 
 }
