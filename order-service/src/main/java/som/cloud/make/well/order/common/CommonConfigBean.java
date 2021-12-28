b4b7f1cecbb3336a53706d707b0c21b615798cec
chengcheng <shmily_think@126.com>
2021-12-28 20:35:25 +0800

    Spring Cloud LoadBalancer修改默认负载均衡规则

--- order-service/src/main/java/som/cloud/make/well/order/common/CommonConfigBean.java
+++ order-service/src/main/java/som/cloud/make/well/order/common/CommonConfigBean.java
@@ -1,12 +1,29 @@
 package som.cloud.make.well.order.common;
 
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@LoadBalancerClient(value = "user-service", configuration = CommonConfigBean.class)
 public class CommonConfigBean {
+
+    @Bean
+    ReactorLoadBalancer<ServiceInstance> randomLoadBalancer(Environment environment,
+                                                            LoadBalancerClientFactory loadBalancerClientFactory) {
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+        return new RandomLoadBalancer(loadBalancerClientFactory
+                .getLazyProvider(name, ServiceInstanceListSupplier.class),
+                name);
+    }
 
     @Bean
     @LoadBalanced
