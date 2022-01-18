bf2e0eb33aad4f7e8cfdbd897a5c83f8df68413f
chengcheng <shmily_think@126.com>
2022-01-18 23:01:27 +0800

    spring cloud gateway

---
+++ gateway/src/main/java/som/cloud/make/well/gateway/WellGatewayApplication.java
@@ -0,0 +1,13 @@
+package som.cloud.make.well.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WellGatewayApplication.class, args);
+    }
+
+}
